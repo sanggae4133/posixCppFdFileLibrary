e8abcf5010d2a27496ed4236ce14bd57fe4a7dbb
Dustbox <53044223+sanggae4133@users.noreply.github.com>
2026-01-14 16:16:48 +0900

    chore: clear dump files

--- records/A.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// Backward-compat header (prefer `examples/records/A.hpp`)
-#include "../examples/records/A.hpp"
--- records/B.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// Backward-compat header (prefer `examples/records/B.hpp`)
-#include "../examples/records/B.hpp"
